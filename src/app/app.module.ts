346db20f573e1f33922b7f98921a27a72fd6bc1f
Heerbarot <barotheer2097@gmail.com>
2019-11-09 08:26:30 +0530

    Modal designing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,16 +15,16 @@ import { AppComponent } from './app.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app.routing.module';
-import {MatSelectModule, MatDialogModule , MatSidenavModule, MatButtonModule,MatCheckboxModule,MatPaginatorModule,MatToolbarModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule, MatIconModule} from '@angular/material';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatSelectModule, MatDialogModule, MatSidenavModule, MatButtonModule, MatCheckboxModule, MatPaginatorModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatMenuModule, MatIconModule, MatDatepickerModule, MatNativeDateModule} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { JobsComponent } from './jobs/jobs.component';
 import { InstructorsComponent } from './instructors/instructors.component';
 import { CoursesComponent } from './courses/courses.component';
 import { SchedulerComponent } from './scheduler/scheduler.component';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 import { SideNavServiceService } from './services/side-nav-service.service';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AddCourseModalComponent } from './courses/add-course-modal/add-course-modal.component';
 import { EditCourseModalComponent } from './courses/edit-course-modal/edit-course-modal.component';
@@ -51,10 +51,10 @@ import { EditJobModalComponent } from './jobs/edit-job-modal/edit-job-modal.comp
     MDBBootstrapModule.forRoot(),
     MatListModule,
     MatPaginatorModule,
-    MatSelectModule,MatButtonModule,MatCheckboxModule,MatToolbarModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule, MatIconModule,
+    MatSelectModule, MatButtonModule, MatCheckboxModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatMenuModule, MatIconModule, MatDatepickerModule, MatNativeDateModule,
     MatSidenavModule,
     AppRoutingModule,
-    FormsModule, 
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     CalendarModule.forRoot({
@@ -66,7 +66,7 @@ import { EditJobModalComponent } from './jobs/edit-job-modal/edit-job-modal.comp
     NgbModule,
   ],
   entryComponents:[AddCourseModalComponent, EditCourseModalComponent,AddJobModalComponent],
-  providers: [SideNavServiceService],
+  providers: [SideNavServiceService, MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 
--- src/app/jobs/add-job-modal/add-job-modal.component.ts
+++ src/app/jobs/add-job-modal/add-job-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-add-job-modal',
@@ -7,8 +8,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddJobModalComponent implements OnInit {
 
-  constructor() { }
+  addJobForm;
+  courseDates = [];
+  startingDate;
+  tempDates = [];
+  temp;
 
+  frequencyDays = [
+    { day: 'Sunday' },
+    { day: 'Monday'},
+    { day: 'Tuesday' },
+    { day: 'Wednesday' },
+    { day: 'Thursday' },
+    { day: 'Friday' },
+    { day: 'Saturday' }
+  ];
+
+  constructor(public formBuilder: FormBuilder) {
+    this.addJobForm = this.formBuilder.group({
+      client : new FormControl(''),
+      instructor: new FormControl(''),
+      location: new FormControl(''),
+      course: new FormControl(''),
+      date: new FormControl(''),
+      frequency: new FormArray([])
+    });
+    this.addCheckboxes();
+   }
+
+  private addCheckboxes() {
+    this.frequencyDays.forEach(() => {
+      const control = new FormControl();
+      (this.addJobForm.controls.frequency as FormArray).push(control);
+    });
+  }
+
+  searchDays($event){
+    this.startingDate = $event.value;
+    console.log(this.startingDate)
+  }
+
+  onCheckChange(event, dayOfTheWeek){
+
+    console.log(event, dayOfTheWeek)
+    console.log("DATE IS", this.startingDate.getDay())
+
+    if(event == true){
+      let tempDate: Date = this.startingDate;
+      console.log('if event is true the startingDate is', this.startingDate)
+      for (var i = 0; i < 10; i++) {
+        let days = (7 - tempDate.getDay() + dayOfTheWeek)
+        let nextDay = new Date(tempDate.setDate(tempDate.getDate() + days))
+        tempDate = nextDay;
+        this.courseDates.push(tempDate);
+      }
+    }
+
+    else {
+      console.log('EVENT IS FALSE')
+      console.log(event, dayOfTheWeek)
+      let tempDate: Date = this.startingDate;
+      console.log('if event is FALSE the tempDate is', this.startingDate)
+      for (var i = 0; i < 10; i++) {
+        let days = (7 - tempDate.getDay() + dayOfTheWeek)
+        let nextDay = new Date(tempDate.setDate(tempDate.getDate() + days))
+        tempDate = nextDay;
+        for(var j = 0; j < this.courseDates.length; j++){
+           if(this.courseDates[j].getDay() == tempDate.getDay()){
+            this.courseDates.splice(j,1)
+          }
+      }
+      console.log("THE ARRAY IS", this.courseDates)
+    }
+  }
+  }
   ngOnInit() {
   }
 
