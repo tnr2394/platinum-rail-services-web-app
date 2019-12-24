c2512518177bd1ad181568b6b110fb43755a18eb
vanashree44771 <vanashree24@gmail.com>
2019-12-24 19:35:08 +0530

    learner allotement tile changes

--- src/app/learners/learner-allotment-tile/learner-allotment-tile.component.ts
+++ src/app/learners/learner-allotment-tile/learner-allotment-tile.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FileService } from '../../services/file.service';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-learner-allotment-tile',
   templateUrl: './learner-allotment-tile.component.html',
   styleUrls: ['./learner-allotment-tile.component.scss']
 })
 export class LearnerAllotmentTileComponent implements OnInit {
-
   @Input('learner') learner: any;
-
-  constructor(public _fileService: FileService) { }
-
+  constructor(public _fileService: FileService, private activatedRoute: ActivatedRoute) { }
+  allotmentId;
+  fileList;
   ngOnInit() {
-    console.log("file tile initialized file= ");
+    this.activatedRoute.params.subscribe(params => {
+      console.log(params['id']);
+      this.allotmentId = params['id'];
+    });
+    console.log("file tile initialized file= ", this.allotmentId);
+    this.getAssignmentFileUsingAllotmentId(this.allotmentId);
   }
-
-  // getFiles(){
-    // this._fileService.getFilesByMaterial()
-  // }
-
+  getAssignmentFileUsingAllotmentId(allotmentId) {
+    console.log("getAssignmentFileUsingAllotmentId= ", allotmentId);
+    this._fileService.getAssignmentFileUsingAllotmentId(allotmentId).subscribe(data => {
+      this.fileList = data;
+      console.log(' this.fileList ======>>>>>>>>>', this.fileList);
+    })
+  }
 }
-
