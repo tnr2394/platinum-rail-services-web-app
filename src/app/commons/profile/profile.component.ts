e682c2acf5baebab30181bba0e4e984b56b009dd
Heerbarot <barotheer2097@gmail.com>
2020-01-22 14:51:26 +0530

    changes in profile

--- src/app/commons/profile/profile.component.ts
+++ src/app/commons/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { JobService } from '../../services/job.service'
+import { ActivatedRoute, Router } from '@angular/router';
+import { LearnerService } from 'src/app/services/learner.service';
 
 @Component({
   selector: 'app-profile',
@@ -13,25 +15,39 @@ export class ProfileComponent implements OnInit {
   client: any;
   location: any;
   course: any;
+  learner: any;
+  name: any;
+  email: any;
+  mobile: any;
 
-  constructor(private _jobService: JobService) { }
+  constructor(private _jobService: JobService, public _learnerService: LearnerService,
+    private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    this.currentUser = JSON.parse(localStorage.currentUser);
-    console.log("Current User", this.currentUser);
-    this.getJob()
+    // GETTING LEARNER ID
+    this.activatedRoute.params.subscribe(params => {
+      console.log("ID-----",params['id']);
+      this._learnerService.getLearner(params['id']).subscribe(learner=>{
+        this.learner = learner.pop();
+        console.log("learner", learner);  
+        this.name = this.learner.name;
+        this.email = this.learner.email;
+        this.mobile = this.learner.mobile;
+        this.getJob(this.learner.job._id);
+      })
+      
+    })
+    console.log("The learner is", this.learner);
   }
 
-  getJob(){
-    this._jobService.getJobById(this.currentUser.job).subscribe((jobRecieved)=>{
+  getJob(id){
+    this._jobService.getJobById(id).subscribe((jobRecieved)=>{
       console.log("Job Recieved", jobRecieved);
       this.job = jobRecieved.pop();
       this.client = this.job.client.name;
       this.location = this.job.location.title;
       this.course = this.job.location.title;
       this.instructor = this.job.instructors;
-      // console.log("=====This.job=====", this.job);
-      
     })
   }
 
