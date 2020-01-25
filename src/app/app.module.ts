88d632ac30136812fcc629bbcfb66338673b0990
Heerbarot <barotheer2097@gmail.com>
2020-01-25 12:19:56 +0530

    tooltip added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { SearchPipe } from './search.pipe';
 
 
 
-
 // Calendar
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
