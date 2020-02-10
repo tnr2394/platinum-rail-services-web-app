4aae33692e20a044ba47ad548f1bef972d9ddd1e
vishalpankhaniya <vishal.pankhaniya786@gmail.com>
2020-02-10 19:24:25 +0530

    Changes in file upload

--- src/app/files/new-file-modal/new-file-modal.component.ts
+++ src/app/files/new-file-modal/new-file-modal.component.ts
@@ -17,6 +17,7 @@ export class NewFileModalComponent implements OnInit {
   uploadedFiles = [];
   uploadedFilesCount = 0
   queueCount = 0
+  uploadStatus = true
   constructor(public uploader: FileUploaderService, public _snackbar: MatSnackBar, public dialogRef: MatDialogRef<any>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
@@ -24,7 +25,7 @@ export class NewFileModalComponent implements OnInit {
     console.log("Upload files initialized", this.data);
 
     this.queue = this.uploader.queue;
-    this.queueCount = this.queue.source['_value'].length || 0 
+    this.queueCount = this.queue.source['_value'].length || 0
     this.uploader.bodyData = this.data;
     this.uploader.onCompleteItem = this.completeItem;
     this.uploader.clearQueue()
@@ -33,11 +34,18 @@ export class NewFileModalComponent implements OnInit {
   completeItem = (item: FileQueueObject, response: any) => {
     console.log('New file uploaded', response, item);
     this.data = response.data;
-    this.uploadedFilesCount++
+
+    if (this.uploadStatus) {
+      this.uploadStatus = false
+    } else {
+      this.uploadedFilesCount++
+    }
     console.log("response.data", response.data);
     this.uploadedFiles.push(response.data.file[0])
     console.log("this.uploadedFiles", this.uploadedFiles);
-    
+    this.queueCount = this.queue.source['_value'].length || 0
+    console.log("uploadedFilesCount", this.uploadedFilesCount)
+    console.log("queueCount", this.queueCount)
     // this.dialogRef.close(response.data.file);
     // this.onCompleteItem.emit({ item, response });
   }
@@ -45,33 +53,35 @@ export class NewFileModalComponent implements OnInit {
   addToQueue() {
     const fileBrowser = this.fileInput.nativeElement;
     this.uploader.addToQueue(fileBrowser.files);
-    console.log("que",this.queue)
+    console.log("que", this.queue)
     // this.dialogRef.close(this.queue)
   }
 
   dragQueue(event) {
     this.uploader.addToQueue(event);
   }
-  upload(){
+  upload() {
     let close
+
+    if (this.uploadStatus && this.uploadedFilesCount == 0) this.uploadedFilesCount++
     // let tempArray = this.uploader.uploadAll()
     // console.log("TEMP array on custom Upload", tempArray);
     // console.log("this.queue", this.queue);
     // this.dialogRef.close(tempArray)
     let temp = this.uploader.uploadAll()
-      // if(tempArray){
-      //   tempArray.forEach(testFile=>{
-      //     console.log("file.response", testFile, testFile.file, testFile.status, testFile['response'] );
-      //     console.log("file.response", JSON.stringify(testFile.response));
-      //     console.log("file.response", JSON.stringify(testFile, null, 2));
+    // if(tempArray){
+    //   tempArray.forEach(testFile=>{
+    //     console.log("file.response", testFile, testFile.file, testFile.status, testFile['response'] );
+    //     console.log("file.response", JSON.stringify(testFile.response));
+    //     console.log("file.response", JSON.stringify(testFile, null, 2));
 
-      //     close = testFile.response ? true : false
-      //     console.log("Close", close);
-      //   })
-      // }
-      // if(close == true) this.dialogRef.close(tempArray)
+    //     close = testFile.response ? true : false
+    //     console.log("Close", close);
+    //   })
+    // }
+    // if(close == true) this.dialogRef.close(tempArray)
   }
-  close(){
+  close() {
     this.dialogRef.close(this.uploadedFiles)
   }
 }
